ee91bd2dca0e1972cb79042e97cc4c1eb9d3e069
isahlabbo <isahlabbo22@gmail.com>
2021-01-05 07:13:52 +0100

    modify the admin registration controller

--- Modules/Admin/Http/Controllers/Auth/AdminRegistrationController.php
+++ Modules/Admin/Http/Controllers/Auth/AdminRegistrationController.php
@@ -10,9 +10,9 @@ use Modules\Apparent\Entities\State;
 use Modules\Apparent\Entities\Address;
 use Modules\Admin\Entities\DesignType;
 use Modules\Client\Entities\Gender;
-use Modules\Apparent\Services\AddressHandle;
 use Illuminate\Support\Facades\Validator;
 use App\Services\Upload\FileUpload;
+use Modules\Apparent\Services\AddressHandle;
 use Modules\Admin\Http\Requests\AdminRegistrationFormRequest as FormRequest;
 
 class AdminRegistrationController extends Controller
@@ -36,7 +36,7 @@ class AdminRegistrationController extends Controller
                 
         $address = new AddressHandle($request->all());
 
-        $address->address->create($request->all());
+        $address->address->newAdmin($request->all());
 
         return redirect()->route('admin.login')->withSuccess('Admin Registered successfully');
 

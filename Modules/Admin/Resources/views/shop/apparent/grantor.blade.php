131d191c2e80581b002c09d0cad83459086efd30
isahlabbo <isahlabbo22@gmail.com>
2021-01-10 11:59:24 +0100

    finishes the shop apparent issues

--- Modules/Admin/Resources/views/shop/apparent/grantor.blade.php
+++ Modules/Admin/Resources/views/shop/apparent/grantor.blade.php
@@ -7,63 +7,78 @@
             <img src="{{storage_url($apparentProgrammeClass->apparent->grantor->image ?? '')}}" height="200" width="200">	
             </div>
             <div class="modal-body">
-            	<table>
-	            	<tr>
-	            		<td>Name</td>
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->name ?? ''}}</td>
-                     </tr>
-                     <tr>
-                     	<td>Email</td>
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->email ?? ''}}</td>
-	                </tr>
-                    <tr>
-                        <td>Phone</td>    
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->phone ?? ''}}</td>
-	                </tr>
-                    <tr>
-                        <td>Gender</td>    
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->gender->name ?? ''}}</td>
-	                </tr>
-                    <tr>
-                        <td>Country</td>    
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->town->lga->state->country->name ?? ''}}</td>
-	                </tr>
-                    <tr>
-                        <td>State</td>    
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->town->lga->state->name ?? ''}}</td>
-	                </tr>
-                    <tr>
-                        <td>Local Government</td>    
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->town->lga->name ?? ''}}</td>
-	                </tr>
-	                <tr>
-                        <td>Town</td>    
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->town->name ?? ''}}</td>
-	                </tr>
-	                <tr>
-                        <td>Area</td>    
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->name ?? ''}}</td>
-	                </tr>
+                <div class="car">
+                    <div class="card-header card-header-primary">Grantor information</div>
+                    <div class="card-body">
+                    	<table>
+        	            	<tr>
+        	            		<td>Name</td>
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->name ?? ''}}</td>
+                             </tr>
+                             <tr>
+                             	<td>Email</td>
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->email ?? ''}}</td>
+        	                </tr>
+                            <tr>
+                                <td>Phone</td>    
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->phone ?? ''}}</td>
+        	                </tr>
+                            <tr>
+                                <td>Gender</td>    
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->gender->name ?? ''}}</td>
+        	                </tr>
+                            <tr>
+                                <td>Country</td>    
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->town->lga->state->country->name ?? ''}}</td>
+        	                </tr>
+                            <tr>
+                                <td>State</td>    
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->town->lga->state->name ?? ''}}</td>
+        	                </tr>
+                            <tr>
+                                <td>Local Government</td>    
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->town->lga->name ?? ''}}</td>
+        	                </tr>
+        	                <tr>
+                                <td>Town</td>    
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->town->name ?? ''}}</td>
+        	                </tr>
+        	                <tr>
+                                <td>Area</td>    
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->area->name ?? ''}}</td>
+        	                </tr>
 
-	                <tr>
-                        <td>Location</td>    
-	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->name ?? ''}}</td>
-	                </tr>
-                </table>     
-                <table>   
-                    <tr>
-                        <td>{{$apparentProgrammeClass->apparent->address->name ?? ''}}</td>
-                    </tr>
-                    <tr>
-                        <td>{{$apparentProgrammeClass->apparent->gender->name ?? ''}}</td>
-                    </tr>
-                    <tr>
-                        <td>{{$apparentProgrammeClass->apparent->religion->name ?? ''}}</td>
-                    </tr>
-                    <tr>
-                        <td>{{$apparentProgrammeClass->apparent->tribe->name ?? ''}}</td>
-                    </tr>
-                </table>
+        	                <tr>
+                                <td>Location</td>    
+        	                    <td>{{$apparentProgrammeClass->apparent->grantor->address->name ?? ''}}</td>
+        	                </tr>
+                        </table>
+                    </div>
+                </div>
+                <br>
+                <div class="car">
+                    <div class="card-header card-header-primary">Apparent information</div>
+                    <div class="card-body">
+                        <table>   
+                            <tr>
+                                <td>Address</td>
+                                <td>{{$apparentProgrammeClass->apparent->address->name ?? ''}}</td>
+                            </tr>
+                            <tr>
+                                <td>Gender</td>
+                                <td>{{$apparentProgrammeClass->apparent->gender->name ?? ''}}</td>
+                            </tr>
+                            <tr>
+                                <td>Religion</td>
+                                <td>{{$apparentProgrammeClass->apparent->religion->name ?? ''}}</td>
+                            </tr>
+                            <tr>
+                                <td>Tribe</td>
+                                <td>{{$apparentProgrammeClass->apparent->tribe->name ?? ''}}</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>     
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
